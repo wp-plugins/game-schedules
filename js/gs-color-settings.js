8406a9af83cebca56d5404d59672d7dbdc7fd924
MarkODonnell <MarkODonnell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-01 04:29:51 +0000

    corrected typo ( cs <-> gs )
    git-svn-id: https://plugins.svn.wordpress.org/game-schedules/trunk@780708 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/gs-color-settings.js
@@ -0,0 +1,27 @@
+//Set up the color pickers to work with our text input field
+jQuery(document).ready(function($){
+
+	$('#gs_main_text_color').wpColorPicker();
+	
+	$('#gs_table_links_color').wpColorPicker();
+	
+	$('#gs_table_title_text_color').wpColorPicker();
+	
+	$('#gs_table_head_bkgd_color').wpColorPicker();
+	$('#gs_table_head_text_color').wpColorPicker();
+	
+    $('#gs_table_even_row_color').wpColorPicker();
+	$('#gs_table_even_row_bkgd').wpColorPicker();
+	
+	$('#gs_table_odd_row_bkgd').wpColorPicker();
+	$('#gs_table_odd_row_color').wpColorPicker();
+	
+	$('#gs_main_bkgd_color').wpColorPicker();
+	$('#gs_main_text_color').wpColorPicker();
+	
+	$('#gs_bio_border_color').wpColorPicker();
+	$('#gs_bio_header_color').wpColorPicker();
+	$('#gs_bio_bkgd_color').wpColorPicker();
+	$('#gs_bio_text_color').wpColorPicker();
+	
+});
