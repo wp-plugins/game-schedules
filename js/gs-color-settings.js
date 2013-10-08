00162cb0bca79fc1ed2a784ecd95b630e0d43586
MarkODonnell <MarkODonnell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-08 00:07:04 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/game-schedules/trunk@784241 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/gs-color-settings.js
+++ js/gs-color-settings.js
@@ -1,27 +1,35 @@
 //Set up the color pickers to work with our text input field
 jQuery(document).ready(function($){
 
-	$('#gs_main_text_color').wpColorPicker();
+	$('#gs_tbl_hdr_bkgd_color').wpColorPicker();
+	$('#gs_tbl_hdr_text_color').wpColorPicker();
 	
-	$('#gs_table_links_color').wpColorPicker();
+	$('#gs_tbl_odd_text_color').wpColorPicker();
+	$('#gs_tbl_odd_bkgd_color').wpColorPicker();
 	
-	$('#gs_table_title_text_color').wpColorPicker();
+	$('#gs_tbl_even_text_color').wpColorPicker();
+	$('#gs_tbl_even_bkgd_color').wpColorPicker();
 	
-	$('#gs_table_head_bkgd_color').wpColorPicker();
-	$('#gs_table_head_text_color').wpColorPicker();
+	$('#gs_tbl_home_text_color').wpColorPicker();
+	$('#gs_tbl_home_bkgd_color').wpColorPicker();
 	
-    $('#gs_table_even_row_color').wpColorPicker();
-	$('#gs_table_even_row_bkgd').wpColorPicker();
+	$('#gs_cdt_bkgd_color').wpColorPicker();
+	$('#gs_cdt_game_time_color').wpColorPicker();
+	$('#gs_cdt_opponent_color').wpColorPicker();
+	$('#gs_cdt_location_color').wpColorPicker();
+	$('#gs_cdt_intro_color').wpColorPicker();
+	$('#gs_cdt_countdown_color').wpColorPicker();
+	$('#gs_cdt_countdown_bkgd_color').wpColorPicker();
 	
-	$('#gs_table_odd_row_bkgd').wpColorPicker();
-	$('#gs_table_odd_row_color').wpColorPicker();
 	
-	$('#gs_main_bkgd_color').wpColorPicker();
-	$('#gs_main_text_color').wpColorPicker();
-	
-	$('#gs_bio_border_color').wpColorPicker();
-	$('#gs_bio_header_color').wpColorPicker();
-	$('#gs_bio_bkgd_color').wpColorPicker();
-	$('#gs_bio_text_color').wpColorPicker();
-	
+	$('#gs_sldr_game_block_bkgd_color').wpColorPicker();
+	$('#gs_sldr_hdr_bkgd_color').wpColorPicker();
+	$('#gs_sldr_hdr_text_color').wpColorPicker();
+	$('#gs_sldr_hdr_divider_color').wpColorPicker();
+	$('#gs_sldr_game_date_color').wpColorPicker();
+	$('#gs_sldr_game_opponent_color').wpColorPicker();
+	$('#gs_sldr_game_location_color').wpColorPicker();
+	$('#gs_sldr_game_time_color').wpColorPicker();
+	$('#gs_sldr_game_links_color').wpColorPicker();
+
 });
