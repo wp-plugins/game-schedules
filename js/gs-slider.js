eb1ca057c346df5ee6e8667ff4cedafd49cf18e0
MarkODonnell <MarkODonnell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-08 00:06:43 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/game-schedules/trunk@784240 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/gs-slider.js
+++ js/gs-slider.js
@@ -1,18 +1,64 @@
-$(document).ready(function() {  
+$(document).ready(function() {
+        //move the last list item before the first item. The purpose of this is if the user clicks previous he will be able to see the last item.
+        $('#carousel_ul li:first').before($('#carousel_ul li:last'));
+
+        //when user clicks the image for sliding right
+        $('#right_scroll img').click(function(){
+
+            //get the width of the items ( i like making the jquery part dynamic, so if you change the width in the css you won't have o change it here too ) '
+            var item_width = $('#carousel_ul li').outerWidth() + 10;
+
+            //calculate the new left indent of the unordered list
+            var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;
+
+            //make the sliding effect using jquery's anumate function '
+            $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){
+
+                //get the first list item and put it after the last list item (that's how the infinite effects is made) '
+                $('#carousel_ul li:last').after($('#carousel_ul li:first'));
+
+                //and get the left indent to the default -210px
+                $('#carousel_ul').css({'left' : '-210px'});
+            });
+        });
+
+        //when user clicks the image for sliding left
+        $('#left_scroll img').click(function(){
+
+            var item_width = $('#carousel_ul li').outerWidth() + 10;
+
+            /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */
+            var left_indent = parseInt($('#carousel_ul').css('left')) + item_width;
+
+            $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){
+
+            /* when sliding to left we are moving the last item before the first item */
+            $('#carousel_ul li:first').before($('#carousel_ul li:last'));
+
+            /* and again, when we make that change we are setting the left indent of our unordered list to the default -210px */
+            $('#carousel_ul').css({'left' : '-210px'});
+            });
+
+        });
+  });
+
+
+//$(document).ready(function() {  
         //move the last list item before the first item. The purpose of this is if the user clicks previous he will be able to see the last item.  
         //$('#carousel_ul li:first').before($('#carousel_ul li:last'));  
   
         //when user clicks the image for sliding right  
         //$('#right_scroll img').click(function(){  
-		$('.gs-slider-right-arrow').click(function(){  
-  
+		
+		//$('#gs-slider-right-arrow').click(function(){  
+			//alert( "Handler for .click() called." );
             //get the width of the items
-            var item_width = $('gs-slider-area .game-block').outerWidth();  
+            /*var item_width = 187; //$('.game-block').outerWidth();  
   
             //calculate the new left indent of the unordered list  
-            var left_indent = parseInt($('.schedule-slider').css('left')) - item_width; 
+            var left_indent = parseInt($('#schedule-slider').css('left')) - item_width; 
 
-			var sched_width = $(.schedule-slider).outerWidth();
+			var sched_width = $('#schedule-slider').outerWidth();
 			
 			if ( left_indent < sched_width - item_width ) {
 				left_ident = left_indent + item_width;
@@ -20,33 +66,37 @@ $(document).ready(function() {
 			}
 			else {
 				//make the sliding effect using jquery's anumate function '  
-				$('.schedule-slider').animate({'left' : left_indent},{queue:false, duration:500},function(){  
+				$('#schedule-slider').animate({'left' : left_indent},{queue:false, duration:500},function(){  
 	  
 					//get the first list item and put it after the last list item (that's how the infinite effects is made) '  
 					//$('#carousel_ul li:last').after($('#carousel_ul li:first'));  
 	  
-					//and get the left indent to the default -210px  
-					//$('#carousel_ul').css({'left' : '-210px'});  
+					//and set the left indent to the default -210px  
+					$('#schedule-slider').css({'left' : '-900px'});  
 				}); 
 			}
-        });  
-  
+			$('#schedule-slider').css({'background-color' : '#f00'});
+			*/
+        //});
+		
+  /*
         //when user clicks the image for sliding left  
-        $('#left_scroll img').click(function(){  
+        $('#gs-slider-left-arrow').click(function(){  
   
             var item_width = $('#carousel_ul li').outerWidth() + 10;  
   
-            /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */  
+            // same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) 
             var left_indent = parseInt($('#carousel_ul').css('left')) + item_width;  
   
             $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
   
-            /* when sliding to left we are moving the last item before the first item */  
+            // when sliding to left we are moving the last item before the first item   
             $('#carousel_ul li:first').before($('#carousel_ul li:last'));  
   
-            /* and again, when we make that change we are setting the left indent of our unordered list to the default -210px */  
+            // and again, when we make that change we are setting the left indent of our unordered list to the default -210px 
             $('#carousel_ul').css({'left' : '-210px'});  
             });  
   
         });  
-  });  
+		*/
+  //});  
